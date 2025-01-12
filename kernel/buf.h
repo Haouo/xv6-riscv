515167df6620048913e6efdfa86205dea4cad4a0
Haouo <q18131500@gs.ncku.edu.tw>
2025-01-12 11:16:01 -0500

    re-format lots files and add header guards

--- kernel/buf.h
+++ kernel/buf.h
@@ -1,12 +1,16 @@
+#ifndef __BUF_H__
+#define __BUF_H__
+
 struct buf {
-  int valid;   // has data been read from disk?
-  int disk;    // does disk "own" buf?
-  uint dev;
-  uint blockno;
-  struct sleeplock lock;
-  uint refcnt;
-  struct buf *prev; // LRU cache list
-  struct buf *next;
-  uchar data[BSIZE];
+    int valid; // has data been read from disk?
+    int disk;  // does disk "own" buf?
+    uint dev;
+    uint blockno;
+    struct sleeplock lock;
+    uint refcnt;
+    struct buf *prev; // LRU cache list
+    struct buf *next;
+    uchar data[BSIZE];
 };
 
+#endif
